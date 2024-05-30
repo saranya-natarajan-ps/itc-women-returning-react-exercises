fbaaf0d468e586450ae56216526ceed53b921e25
Saranya Natarajan <saranya-natarajan@pluralsight.com>
2024-05-30 13:54:05 +0530

    created hello world component

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './Hello';
+
+function App() {
+  return (
+    <div className="App">
+          Day 1- Exercise -01-create-react-app
+          <Hello></Hello>
+    </div>
+  );
+}
+
+export default App;
